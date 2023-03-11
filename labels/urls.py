4378f5fc3be97d85ba5e00a379e0350f9148df4d
Makeev095 <makeev095095@mail.ru>
2023-03-11 17:51:21 +0300

    --all

--- labels/urls.py
+++ labels/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from labels.views import CreateLabelView, UpdateLabelView, DeleteLabelView, Index
 
 urlpatterns = [
-    path('', Index, name='labels'),
+    path('', Index.as_view(), name='labels'),
     path('create/', CreateLabelView.as_view(), name='create_label'),
     path('<int:pk>/update/', UpdateLabelView.as_view(), name='update_label'),
     path('<int:pk>/delete/', DeleteLabelView.as_view(), name='delete_label'),
