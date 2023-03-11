745291fcb08e620dbd032ae3cd96055fc91478ac
Makeev095 <makeev095095@mail.ru>
2023-03-11 17:45:41 +0300

    --all

--- labels/views.py
+++ labels/views.py
@@ -10,7 +10,7 @@ from django.utils.translation import gettext_lazy as _
 from task_manager.mixin import AuthRequiredMixin
 
 
-class LabelIndex(AuthRequiredMixin,SuccessMessageMixin):
+class LabelIndex(AuthRequiredMixin,SuccessMessageMixin,Label,LabelForm):
     def index(request):
         labels = Label.objects.all()
         return render(request, 'labels/index.html', {'labels': labels})
